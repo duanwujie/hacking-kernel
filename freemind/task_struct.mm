ad2a44995ed82dcc284b552ac99cf1823d50ebeb
duanwujie <dhacklove@163.com>
2016-08-30 14:03:46 +0800

    add free mind

---
+++ freemind/task_struct.mm
@@ -0,0 +1,16 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1472534183745" ID="ID_250480919" MODIFIED="1472534187246" TEXT="task_struct">
+<node CREATED="1472534192500" ID="ID_1464511732" MODIFIED="1472534810460" POSITION="right" TEXT="state&#xff1a;&#x6807;&#x8bc6;&#x8be5;&#x8fdb;&#x7a0b;&#x7684;&#x72b6;&#x6001;"/>
+<node CREATED="1472534200160" ID="ID_1249917689" MODIFIED="1472534245472" POSITION="right" TEXT="void * stack&#xff1a;&#x5728;do_fork&#x7684;&#x65f6;&#x5019;&#x88ab;&#x521d;&#x59cb;&#x5316;&#x6307;&#x5411;&#x4e86;thread_info"/>
+<node CREATED="1472535122991" ID="ID_956475928" MODIFIED="1472535154366" POSITION="right" TEXT="children&#xff1a;&#x94fe;&#x8868;&#xff0c;&#x8be5;&#x8fdb;&#x7a0b;&#x7684;&#x6240;&#x6709;&#x5b50;&#x8fdb;&#x7a0b;"/>
+<node CREATED="1472535179944" ID="ID_765656150" MODIFIED="1472535195396" POSITION="right" TEXT="sibling&#xff1a;&#x94fe;&#x8868;&#xff0c;&#x8be5;&#x8fdb;&#x7a0b;&#x7684;&#x6240;&#x6709;&#x5144;&#x5f1f;&#x8fdb;&#x7a0b;"/>
+<node CREATED="1472535293199" ID="ID_121720525" MODIFIED="1472535314510" POSITION="right" TEXT="alloc_lock&#xff1a;&#x5206;&#x914d;&#x9501;&#xff0c;&#x7528;&#x4e8e;&#x4fdd;&#x62a4; mm, files, fs, tty, keyrings, mems_allowed&#x7b49;&#x5206;&#x914d;"/>
+<node CREATED="1472535362390" ID="ID_135621553" MODIFIED="1472535366721" POSITION="right" TEXT="pending&#xff1a;&#x4fe1;&#x53f7;&#x76f8;&#x5173;"/>
+<node CREATED="1472535452264" ID="ID_930608173" MODIFIED="1472535521235" POSITION="right" TEXT=" utime&#xff1a;&#x7528;&#x4e8e;&#x8bb0;&#x5f55;&#x8fdb;&#x7a0b;&#x5728;&#x7528;&#x6237;&#x6001;&#x4e0b;&#x6240;&#x7ecf;&#x8fc7;&#x7684;ticks"/>
+<node CREATED="1472535482987" ID="ID_508772359" MODIFIED="1472535513630" POSITION="right" TEXT="stime&#xff1a;&#x7528;&#x4e8e;&#x8bb0;&#x5f55;&#x8fdb;&#x7a0b;&#x5728;&#x5185;&#x6838;&#x6001;&#x4e0b;&#x6240;&#x7ecf;&#x8fc7;&#x7684;ticks"/>
+<node CREATED="1472535591099" ID="ID_263161937" MODIFIED="1472535592615" POSITION="right" TEXT="nvcsw/nivcsw&#x662f;&#x81ea;&#x613f;&#xff08;voluntary&#xff09;/&#x975e;&#x81ea;&#x613f;&#xff08;involuntary&#xff09;&#x4e0a;&#x4e0b;&#x6587;&#x5207;&#x6362;&#x8ba1;"/>
+<node CREATED="1472535625664" ID="ID_576046253" MODIFIED="1472535626860" POSITION="right" TEXT="start_time&#x548c;real_start_time&#x90fd;&#x662f;&#x8fdb;&#x7a0b;&#x521b;&#x5efa;&#x65f6;&#x95f4;&#xff0c;real_start_time&#x8fd8;&#x5305;&#x542b;&#x4e86;&#x8fdb;&#x7a0b;&#x7761;&#x7720;&#x65f6;&#x95f4;&#xff0c;&#x5e38;&#x7528;&#x4e8e;/proc/pid/stat"/>
+<node CREATED="1472535671555" ID="ID_422008275" MODIFIED="1472535727617" POSITION="right" TEXT="prev_cputime&#x662f;&#x5148;&#x524d;&#x7684;&#x8fd0;&#x884c;&#x65f6;&#x95f4;"/>
+</node>
+</map>
