fa225632cdc95dc2884d3049f9b4e767666e9517
duanwujie <dhacklove@163.com>
2016-08-30 14:21:52 +0800

    Add the freemind files

---
+++ freemind/mm_struct.mm
@@ -0,0 +1,10 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1472537074531" ID="ID_953081463" MODIFIED="1472537087068" TEXT="mm_struct">
+<node CREATED="1472537287700" ID="ID_1387813448" MODIFIED="1472537297370" POSITION="right" TEXT="mmap&#xff1a;VMA&#x7684;&#x94fe;&#x8868;"/>
+<node CREATED="1472537385078" ID="ID_524402388" MODIFIED="1472537400568" POSITION="right" TEXT="mm_rb&#xff1a;VMA&#x7684;&#x7ea2;&#x9ed1;&#x6811;"/>
+<node CREATED="1472537092840" ID="ID_586650181" MODIFIED="1472537103836" POSITION="right" TEXT="page_table_lock&#xff1a;&#x7528;&#x4e8e;&#x4fdd;&#x62a4;&#x9875;&#x8868;&#x7684;&#x9501;"/>
+<node CREATED="1472537622512" ID="ID_1241468486" MODIFIED="1472537670747" POSITION="right" TEXT="mm_users&#xff1a;&#x539f;&#x5b50;&#x53d8;&#x91cf;&#xff0c;&#x8ba1;&#x6570;&#x5668;&#x8bb0;&#x5f55;&#x4e86;&#x4f7f;&#x7528;mm_stuct&#x7684;&#x7528;&#x6237;&#x6570;"/>
+<node CREATED="1472537691132" ID="ID_949092566" MODIFIED="1472537709351" POSITION="right" TEXT="mm_count&#xff1a;&#x539f;&#x5b50;&#x53d8;&#x91cf;&#xff0c;&#x8be5;&#x7ed3;&#x6784;&#x7684;&#x5f15;&#x7528;&#x8ba1;&#x6570;"/>
+</node>
+</map>
