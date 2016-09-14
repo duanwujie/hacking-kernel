edc452c1690373d2e3f9bff8bb1908dde9e06025
duanwujie <dhacklove@163.com>
2016-09-14 17:26:01 +0800

    Blessing EA Understand

---
+++ Blessing/sys_mount.c
@@ -0,0 +1,13 @@
+
+//linux-4.3.3/fs/namespace.c
+
+/**
+ * sys_mount - 挂载文件系统
+ * @dev_name:设备名称 
+ * @dir_name:挂载点路径 
+ * @type:文件系统类型
+ * @flags:标志位供do_mount调用。
+ * @data:选项信息
+ */
+SYSCALL_DEFINE5(mount, char __user *, dev_name, char __user *, dir_name,
+		char __user *, type, unsigned long, flags, void __user *, data);
